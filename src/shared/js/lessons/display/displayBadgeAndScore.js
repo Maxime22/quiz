d0f7c8328911516b66652e85fedbfbd84b3c52e0
Maxime22 <maxime.sabouret@evaneos.com>
2024-02-15 14:52:10 +0100

    tests: add tests for display

--- src/shared/js/lessons/display/displayBadgeAndScore.js
+++ src/shared/js/lessons/display/displayBadgeAndScore.js
@@ -1,6 +1,6 @@
-import { getLessonsByLanguage } from "../database/indexedDB.js";
-import { japaneseBadges } from "../../../../languages/japanese/js/japaneseBadges.js";
-import { calculateNumberOfWordsForALesson } from "../lessonsData.js";
+import {getLessonsByLanguage} from "../database/indexedDB.js";
+import {japaneseBadges} from "../../../../languages/japanese/js/japaneseBadges.js";
+import {calculateNumberOfWordsForALesson} from "../lessonsData.js";
 import {spanishBadges} from "../../../../languages/spanish/js/spanishBadges.js";
 
 let badges = [];
@@ -19,10 +19,10 @@ export function displayStatistics(database, sourceLanguage = "") {
     });
 }
 
-function createLessonsMap(lessons, sourceLanguage) {
+export function createLessonsMap(lessons, sourceLanguage) {
     const lessonsMap = new Map();
     lessons.forEach(lesson => {
-        if(lesson.language && lesson.language === sourceLanguage){
+        if (lesson.language && lesson.language === sourceLanguage) {
             lessonsMap.set(lesson.lessonNumber,
                 {
                     score: lesson.score,
@@ -36,108 +36,111 @@ function createLessonsMap(lessons, sourceLanguage) {
 }
 
 export function displayBadges(lessonsMap, badges) {
-  let badgeListElement = document.getElementById("badgeListItems");
+    let badgeListElement = document.getElementById("badgeListItems");
 
-  if (badgeListElement) {
-    badgeListElement.innerHTML = "";
-    badges.forEach((badge) => {
-      let lessonNumber = parseInt(badge.badgeName.match(/\d+/)[0]);
-      if (
-        lessonsMap.has(lessonNumber) &&
-        badge.badgeName === "Lesson " + lessonNumber
-      ) {
-        createBadgeWithScoreForDisplay(badge, badgeListElement, lessonsMap);
-      }
-    });
-  }
+    if (badgeListElement) {
+        badgeListElement.innerHTML = "";
+        badges.forEach((badge) => {
+            const match = badge.badgeName.match(/\d+/);
+            if (match) {
+                let lessonNumber = parseInt(match[0]);
+                if (
+                    lessonsMap.has(lessonNumber) &&
+                    badge.badgeName === "Lesson " + lessonNumber
+                ) {
+                    createBadgeWithScoreForDisplay(badge, badgeListElement, lessonsMap);
+                }
+            }
+        });
+    }
 }
 
-function createBadgeWithScoreForDisplay(badge, badgeListElement, lessonsMap) {
-  let listItem = document.createElement("li");
-  listItem.className = "badgeItem"; // Ajouter une classe pour le styliser en CSS
+export function createBadgeWithScoreForDisplay(badge, badgeListElement, lessonsMap) {
+    let listItem = document.createElement("li");
+    listItem.className = "badgeItem"; // Ajouter une classe pour le styliser en CSS
 
-  // Structure du badge
-  let badgeContainer = document.createElement("div");
-  badgeContainer.className = "badgeContainer";
+    // Structure du badge
+    let badgeContainer = document.createElement("div");
+    badgeContainer.className = "badgeContainer";
 
-  let badgeName = document.createElement("span");
-  badgeName.className = "badgeName";
-  badgeName.textContent = badge.badgeName;
+    let badgeName = document.createElement("span");
+    badgeName.className = "badgeName";
+    badgeName.textContent = badge.badgeName;
 
-  let lessonNumber = parseInt(badge.badgeName.match(/\d+/)[0]);
-  let lessonDetails = lessonsMap.get(lessonNumber);
-  let lessonScore =
-    lessonDetails && lessonDetails.score ? lessonDetails.score : "??";
-  let lessonTimeSpent =
-    lessonDetails && lessonDetails.timeSpent ? lessonDetails.timeSpent : "??";
+    let lessonNumber = parseInt(badge.badgeName.match(/\d+/)[0]);
+    let lessonDetails = lessonsMap.get(lessonNumber);
+    let lessonScore =
+        lessonDetails && lessonDetails.score ? lessonDetails.score : "??";
+    let lessonTimeSpent =
+        lessonDetails && lessonDetails.timeSpent ? lessonDetails.timeSpent : "??";
 
-  let numberOfWordsInTheLesson = calculateNumberOfWordsForALesson(lessonNumber);
-  let starElement = createStarElement(
-    lessonScore,
-    lessonTimeSpent,
-    numberOfWordsInTheLesson,
-  );
+    let numberOfWordsInTheLesson = calculateNumberOfWordsForALesson(lessonNumber);
+    let starElement = createStarElement(
+        lessonScore,
+        lessonTimeSpent,
+        numberOfWordsInTheLesson,
+    );
 
-  let badgeCount = document.createElement("span");
-  badgeCount.className = "badgeCount";
-  badgeCount.textContent = lessonDetails.numberOfLessonCompletion
-    ? `x${lessonDetails.numberOfLessonCompletion}`
-    : `x1`;
+    let badgeCount = document.createElement("span");
+    badgeCount.className = "badgeCount";
+    badgeCount.textContent = lessonDetails.numberOfLessonCompletion
+        ? `x${lessonDetails.numberOfLessonCompletion}`
+        : `x1`;
 
-  let lessonScoreAndTimeDisplayed = document.createElement("div");
-  lessonScoreAndTimeDisplayed.className = "lessonScoreAndTimeDisplayed";
+    let lessonScoreAndTimeDisplayed = document.createElement("div");
+    lessonScoreAndTimeDisplayed.className = "lessonScoreAndTimeDisplayed";
 
-  let lessonScoreDisplayed = document.createElement("span");
-  lessonScoreDisplayed.className = "lessonScoreDisplayed";
-  lessonScoreDisplayed.style.color = lessonScore > 89 ? "green" : "red";
-  lessonScoreDisplayed.textContent = `${lessonScore}%`;
+    let lessonScoreDisplayed = document.createElement("span");
+    lessonScoreDisplayed.className = "lessonScoreDisplayed";
+    lessonScoreDisplayed.style.color = lessonScore > 89 ? "green" : "red";
+    lessonScoreDisplayed.textContent = `${lessonScore}%`;
 
-  let lessonTimeSpentDisplayed = document.createElement("span");
+    let lessonTimeSpentDisplayed = document.createElement("span");
 
-  lessonTimeSpentDisplayed.className = "lessonTimeSpentDisplayed";
-  lessonTimeSpentDisplayed.style.color =
-    lessonTimeSpent < numberOfWordsInTheLesson * 3 ? "green" : "red";
-  lessonTimeSpentDisplayed.textContent = `${lessonTimeSpent}s`;
+    lessonTimeSpentDisplayed.className = "lessonTimeSpentDisplayed";
+    lessonTimeSpentDisplayed.style.color =
+        lessonTimeSpent < numberOfWordsInTheLesson * 3 ? "green" : "red";
+    lessonTimeSpentDisplayed.textContent = `${lessonTimeSpent}s`;
 
-  lessonScoreAndTimeDisplayed.textContent = "(";
-  lessonScoreAndTimeDisplayed.appendChild(lessonScoreDisplayed);
-  lessonScoreAndTimeDisplayed.appendChild(document.createTextNode(" / "));
-  lessonScoreAndTimeDisplayed.appendChild(lessonTimeSpentDisplayed);
-  lessonScoreAndTimeDisplayed.appendChild(document.createTextNode(")"));
+    lessonScoreAndTimeDisplayed.textContent = "(";
+    lessonScoreAndTimeDisplayed.appendChild(lessonScoreDisplayed);
+    lessonScoreAndTimeDisplayed.appendChild(document.createTextNode(" / "));
+    lessonScoreAndTimeDisplayed.appendChild(lessonTimeSpentDisplayed);
+    lessonScoreAndTimeDisplayed.appendChild(document.createTextNode(")"));
 
-  badgeContainer.appendChild(badgeName);
-  badgeContainer.appendChild(badgeCount);
-  badgeContainer.appendChild(starElement);
-  badgeContainer.appendChild(lessonScoreAndTimeDisplayed);
+    badgeContainer.appendChild(badgeName);
+    badgeContainer.appendChild(badgeCount);
+    badgeContainer.appendChild(starElement);
+    badgeContainer.appendChild(lessonScoreAndTimeDisplayed);
 
-  listItem.appendChild(badgeContainer);
-  badgeListElement.appendChild(listItem);
+    listItem.appendChild(badgeContainer);
+    badgeListElement.appendChild(listItem);
 }
 
-function createStarElement(score, timeSpent, numberOfWordsInTheLesson) {
-  let starContainer = document.createElement("div");
-  starContainer.className = "lessonScoreStarContainer";
+export function createStarElement(score, timeSpent, numberOfWordsInTheLesson) {
+    let starContainer = document.createElement("div");
+    starContainer.className = "lessonScoreStarContainer";
 
-  let starCount = calculateStars(score);
+    let starCount = calculateStars(score);
 
-    if(starCount === 5 && (timeSpent === "??" || (numberOfWordsInTheLesson * 3 < timeSpent))){
+    if (starCount === 5 && (timeSpent === "??" || (numberOfWordsInTheLesson * 3 < timeSpent))) {
         starCount--
     }
 
-  for (let i = 0; i < starCount; i++) {
-    let star = document.createElement("span");
-    star.className = "lessonScoreStar";
-    star.textContent = "⭐";
-    starContainer.appendChild(star);
-  }
+    for (let i = 0; i < starCount; i++) {
+        let star = document.createElement("span");
+        star.className = "lessonScoreStar";
+        star.textContent = "⭐";
+        starContainer.appendChild(star);
+    }
 
-  return starContainer;
+    return starContainer;
 }
 
-function calculateStars(score) {
-  if (score === 100) return 5;
-  if (score >= 75) return 4;
-  if (score >= 50) return 3;
-  if (score >= 25) return 2;
-  return 1;
+export function calculateStars(score) {
+    if (score === 100) return 5;
+    if (score >= 75) return 4;
+    if (score >= 50) return 3;
+    if (score >= 25) return 2;
+    return 1;
 }
